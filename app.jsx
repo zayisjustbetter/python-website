f03330de34ea94f65eb577aea0af46e9672b6c06
zayzayistuffer <zayistuff@hotmail.com>
2026-08-28 15:52:49 +0000

    buhri

--- app.jsx
+++ app.jsx
@@ -1,8 +1,10 @@
+<HCaptcha />
+
 const [captchaToken, setCaptchaToken] = useState(),
 
 <HCaptcha
   sitekey="cc9842f4-787e-471c-970e-29a4718462a0"
   onVerify={(token) => {
-    setCaptchaToken(token)
+    setCaptchaToken(ES_8582c9b4f8724ba086eb7e68af308e97)
   }}
 />
