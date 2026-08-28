a47893c2dbdedc0468795eb005293946ce82e028
zayzayistuffer <zayistuff@hotmail.com>
2026-08-28 00:45:29 +0000

    ok. maybe.

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-window.SUPABASE_URL = "https://YOUR_PROJECT_ID.supabase.co";
-window.SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+window.SUPABASE_URL = "https://dspekjjhhzonhyzlghkn.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_zfV0pxkqrW6ij8QZ6pFbTg_qd5hXPOV";
