45c694b69e6ae82b5adaacbded63194d34121789
zayzayistuffer <zayistuff@hotmail.com>
2026-08-28 15:54:06 +0000

    fewf

--- app.jsx
+++ app.jsx
@@ -5,6 +5,6 @@ const [captchaToken, setCaptchaToken] = useState(),
 <HCaptcha
   sitekey="cc9842f4-787e-471c-970e-29a4718462a0"
   onVerify={(token) => {
-    setCaptchaToken(ES_8582c9b4f8724ba086eb7e68af308e97)
+    setCaptchaToken(token)
   }}
 />
